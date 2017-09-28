1e74021dbc4252dd2ca1bf70f6062ec2e9758b7d
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-09-29 01:51:42 +0200

    ability to change network id

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ let utils = require('./script.js');
 let addr = '0x00590d7fbc805b7882788d71afbe7ec2deaf03ca';
 
 utils.setWhitelistPrivkey('');
+// for test network:
+utils.setHttpProvider('https://kovan.infura.io/49cO6Bu58uaoA0tgS2Zi');
+// for mainnet:
+utils.setHttpProvider('https://mainnet.infura.io/49cO6Bu58uaoA0tgS2Zi');
 
 // check if address is whitelisted
 utils.isWhitelisted(addr).then(done => {
